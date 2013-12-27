31050dd40a62a5be66dfa8658344e81ec1aea4c3
Stephan Dilly <dilly.stephan@gmail.com>
2013-12-27 13:48:45 +0100

    use latest vibe.c changes to make sll connect work

--- source/apn/apn.d
+++ source/apn/apn.d
@@ -48,7 +48,9 @@ private:
 		auto conn = connectTCP(m_options.address, m_options.port);
 		logInfo("tcp connected");
 
-		auto sslctx = new SSLContext(m_options.cert, m_options.key);
+		auto sslctx = new SSLContext(SSLContextKind.client);
+		sslctx.useCertificateChainFile(m_options.cert);
+		sslctx.usePrivateKeyFile(m_options.key);
 		auto stream = new SSLStream(conn, sslctx, SSLStreamState.connecting);
 
 		logInfo("ssl enabled");
